3b90532da33db19a1f7c64008029c0a9568b21a2
Diego Fischer <diegofischer.dev@gmail.com>
2026-03-16 09:27:59 +0000

    corrigindo erro de build vps

--- src/sale/sale.module.ts
+++ src/sale/sale.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { SaleService } from './sale.service';
 import { SaleController } from './sale.controller';
 import { PrismaService } from '../prisma/prisma.service';
+import { StripeModule } from '../stripe/stripe.module';
 
 @Module({
+  imports: [StripeModule],
   controllers: [SaleController],
   providers: [SaleService, PrismaService],
 })
